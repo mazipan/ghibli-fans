283ded671d0883a50613a94f4eaf65df57f36533
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-01-02 17:06:26 +0700

    add imdb url

--- src/data/film.js
+++ src/data/film.js
@@ -1,141 +1,141 @@
 export default {
 	"2baf70d1-42bb-4437-b551-e5fed5a87abe": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0092067/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-castle-sky-1986.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BNTg0NmI1ZGQtZTUxNC00NTgxLThjMDUtZmRlYmEzM2MwOWYwXkEyXkFqcGdeQXVyMzM4MjM0Nzg@._V1_UY268_CR2,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/castle-sky-1986/",
 		title: "Castle in the Sky"
 	},
 	"12cfb892-aac0-4c5b-94af-521852e46d6a": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0095327/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-grave-fireflies-1988.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BZjEwNDVhZjMtYzA1MS00ZWUxLThjOGUtZTliNGZiNGYyMjA3XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY268_CR4,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/grave-fireflies-1988/",
 		title: "Grave of the Fireflies"
 	},
 	"58611129-2dbc-4a81-a72f-77ddfc1b1b49": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0096283/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2016/02/film-my-neighbor-totoro-1988.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BNTdiOTQ0YmUtOGE3YS00NDg5LWI3YTEtNDAxZmE0MzRmZWM5L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY268_CR6,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/my-neighbor-totoro-1988/",
 		title: "My Neighbor Totoro"
 	},
 	"ea660b10-85c4-4ae3-8a5f-41cea3648e3e": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0097814/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-kikis-delivery-service-1989.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BOTc0ODM1Njk1NF5BMl5BanBnXkFtZTcwMDI5OTEyNw@@._V1_UY268_CR3,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/kikis-delivery-service-1989/",
 		title: "Kiki's Delivery Service"
 	},
 	"4e236f34-b981-41c3-8c65-f8c9000b94e7": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0102587/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-only-yesterday-1991.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BMTY5NjI2MjQxMl5BMl5BanBnXkFtZTgwMDA2MzM2NzE@._V1_UY268_CR0,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/only-yesterday-1991/",
 		title: "Only Yesterday"
 	},
 	"ebbb6b7c-945c-41ee-a792-de0e43191bd8": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0104652/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2015/12/film-porco-rosso-1992.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BZDIyOTBiZjktYTE0NS00ZGE2LWEzM2YtMzM0MWI2YzIzMGM2L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY268_CR3,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/porco-rosso-1992/",
 		title: "Porco Rosso"
 	},
 	"1b67aa9a-2e4a-45af-ac98-64d6ad15b16c": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0110008",
 		image:
-			"https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Pompokoposter.jpg/220px-Pompokoposter.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BNDM3MDc3OTk4MF5BMl5BanBnXkFtZTcwMzQ2ODIyNw@@._V1_UY268_CR3,0,182,268_AL_.jpg",
 		streaming:
 			"https://indoxxi.net/movie/nonton-pom-poko-1994-subtitle-indonesia-bsj/play",
 		title: "Pom Poko"
 	},
 	"ff24da26-a969-4f0e-ba1e-a122ead6c6e3": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0113824/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-whisper-heart-1995.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BNmQ3N2U5NGMtNjU0MS00YTQzLWE1ZDctZDU5M2M5NTNjOGRmXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY268_CR3,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/whisper-heart-1995/",
 		title: "Whisper of the Heart"
 	},
 	"0440483e-ca0e-4120-8c50-4c8cd9b965d6": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0119698/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2015/12/film-princess-mononoke-1997.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BMTVlNWM4NTAtNDQxYi00YWU5LWIwM2MtZmVjYWFmODZiODE5XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY268_CR4,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/princess-mononoke-1997/",
 		title: "Princess Mononoke"
 	},
 	"45204234-adfd-45cb-a505-a8e7a676b114": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0206013/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-neighbors-yamadas-1999.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BOThkZjMyMGYtMDNjNy00NjcwLTk1NmEtZmQwYTliMmM4YjBhXkEyXkFqcGdeQXVyMzM4NjcxOTc@._V1_UY268_CR3,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/neighbors-yamadas-1999/",
 		title: "My Neighbors the Yamadas"
 	},
 	"dc2e6bd1-8156-4886-adff-b39e6043af0c": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0245429/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-spirited-away-2001.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BOGJjNzZmMmUtMjljNC00ZjU5LWJiODQtZmEzZTU0MjBlNzgxL2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX182_CR0,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/spirited-away-2001/",
 		title: "Spirited Away"
 	},
 	"90b72513-afd4-4570-84de-a56c312fdf81": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0347618/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-cat-returns-2002.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BMTQ5ODMyNTgzOV5BMl5BanBnXkFtZTcwNDM4ODAyNw@@._V1_UY268_CR4,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/cat-returns-2002/",
 		title: "The Cat Returns"
 	},
 	"cd3d059c-09f4-4ff3-8d63-bc765a5184fa": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0347149/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-howls-moving-castle-2004.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BZTRhY2QwM2UtNWRlNy00ZWQwLTg3MjktZThmNjQ3NTdjN2IxXkEyXkFqcGdeQXVyMzg2MzE2OTE@._V1_UY268_CR5,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/howls-moving-castle-2004/",
 		title: "Howl's Moving Castle"
 	},
 	"112c1e67-726f-40b1-ac17-6974127bb9b9": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt0495596/",
 		image:
-			"   http://s0.lk21.top/wp-content/uploads/2015/12/film-tales-from-earthsea-2006.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BMTExODA0NzkxOTZeQTJeQWpwZ15BbWU3MDM0NDEyNzM@._V1_UX182_CR0,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/tales-from-earthsea-2006/",
 		title: "Tales from Earthsea"
 	},
 	"758bf02e-3122-46e0-884e-67cf83df1786": {
-		imdb: "",
-		image: "http://s0.lk21.top/wp-content/uploads/2016/02/film-ponyo-2008.jpg",
+		imdb: "http://www.imdb.com/title/tt0876563/",
+		image: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjA5NzkxNTg2MF5BMl5BanBnXkFtZTcwMTA3MjU1Mg@@._V1_UX182_CR0,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/ponyo-2008/",
 		title: "Ponyo"
 	},
 	"2de9426b-914a-4a06-a3a0-5e6d9d3886f6": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt1568921/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-secret-world-arrietty-2010.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BMTAxNjk3OTYyODReQTJeQWpwZ15BbWU3MDgyODY2OTY@._V1_UX182_CR0,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/secret-world-arrietty-2010/",
-		title: "Arrietty"
+		title: "The Secret World of Arrietty"
 	},
 	"45db04e4-304a-4933-9823-33f389e8d74d": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt1798188/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-poppy-hill-2011.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BYWFiYjE0NTctZThiZC00NTYxLTllOWQtYmMyYzY4NWZiZDYyXkEyXkFqcGdeQXVyMTIyNzY1NzM@._V1_UY268_CR4,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/poppy-hill-2011/",
 		title: "From Up on Poppy Hill"
 	},
 	"67405111-37a5-438f-81cc-4666af60c800": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt2013293/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2016/02/film-the-wind-rises-2013.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BZGZkNWQ1ZDQtMjYzNy00NmYxLWEwMDEtNjY2Y2U2ZWEyOGQ5L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UY268_CR4,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/wind-rises-2013/",
 		title: "The Wind Rises"
 	},
 	"578ae244-7750-4d9f-867b-f3cd3d6fecf4": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt2576852/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-the-tale-of-the-princess-kaguya-2013.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BMTcwODI0MzEwOF5BMl5BanBnXkFtZTgwNjkyNTEwMTE@._V1_UY268_CR4,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/the-tale-of-the-princess-kaguya-2013/",
 		title: "The Tale of the Princess Kaguya"
 	},
 	"5fdfb320-2a02-49a7-94ff-5ca418cae602": {
-		imdb: "",
+		imdb: "http://www.imdb.com/title/tt3398268/",
 		image:
-			"http://s0.lk21.top/wp-content/uploads/2017/11/film-when-marnie-was-there-2014.jpg",
+			"https://images-na.ssl-images-amazon.com/images/M/MV5BYTkyMTNmY2EtOTZmYi00YWU4LTgxN2UtZWU0NTI0OGFkMWRjXkEyXkFqcGdeQXVyMzg2MzE2OTE@._V1_UY268_CR2,0,182,268_AL_.jpg",
 		streaming: "http://lk21.top/when-marnie-was-there-2014/",
 		title: "When Marnie Was There"
 	}
