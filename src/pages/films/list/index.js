f331483eb1524d7dcaf72f4e3cd540df955d281b
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-01-05 09:43:07 +0700

    change to in-feed ads

--- src/pages/films/list/index.js
+++ src/pages/films/list/index.js
@@ -9,6 +9,19 @@ export default class FilmsPage extends Component {
 			<main class={style.home}>
 				<h1>List of Studio Ghibli's films</h1>
 				<Films />
+				{/* ADSENSE IN-FEED */}
+				<script
+					async
+					src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+				/>
+				<ins
+					class="adsbygoogle"
+					style="display:block"
+					data-ad-format="fluid"
+					data-ad-client="ca-pub-5442972248172818"
+					data-ad-slot="2105028631"
+				/>
+				<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 			</main>
 		);
 	}
