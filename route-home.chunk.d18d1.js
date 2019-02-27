c0d828da533191a30eea2889265dc0f38282a6a5
Irfan Maulana <irfan.maulana@bizzy.co.id>
2019-02-27 18:11:44 +0700

    Merge to gh-pages ==> 2/27/2019, 6:11:28 PM

---
+++ route-home.chunk.d18d1.js
@@ -0,0 +1,2 @@
+webpackJsonp([1],{ZAL5:function(e){e.exports={home:"home__MseGd"}},"iOg+":function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var o=t("KM04"),c=(t.n(o),t("ZAL5")),s=t.n(c),u=Object(o.h)("h1",null,"Home"),h=Object(o.h)("p",null,"This is the Home component.");n.default=function(){return Object(o.h)("div",{class:s.a.home},u,h)}}});
+//# sourceMappingURL=route-home.chunk.d18d1.js.map
